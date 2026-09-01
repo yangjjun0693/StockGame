2ff0a5c8b1226df715b8ccca9d7c4cc4d15dff72
Junyoung Yang <chy060513@gmail.com>
2026-09-01 22:50:12 +0900

    fixed tailwindcss error

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        myeongjo: ['Nanum Myeongjo', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
